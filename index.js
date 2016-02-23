9d2999e07fc032fef94c74f24da6f4f93a07ce8e
Ben Frain <bf@benfrain.com>
2016-02-23 21:37:27 +0000

    incorporating ben-eb suggestions and feedback

--- index.js
+++ index.js
@@ -5,8 +5,12 @@ const fs = require("fs");
 const prependFile = require("prepend-file");
 const prettyBytes = require("pretty-bytes");
 
-module.exports = postcss.plugin("postcss-extract-to-file", function myplugin(options) {
+module.exports = postcss.plugin("postcss-extract-to-file", function postcssExtractToFile(options) {
     return function (css) {
+
+        return new Promise(function (resolve, reject) {
+
+        });
 
         options = options || {};
 
@@ -15,60 +19,61 @@ module.exports = postcss.plugin("postcss-extract-to-file", function myplugin(opt
         var remaining = []
 
         css.walkRules(function (rule, index) {
-
             // We check each rule against the possible overrides. If it matches an override it gets moved the the extracted file 
-            if (checkOverrides(rule.selector)) {
+            if (checkOverrides(options.extractors, rule.selector)) {
                 extracted.push(rule);
             }
             // Otherwise it gets written to the 'remaining' rule
             else {
                 remaining.push(rule);
             }
-
-            // Takes a selector as its argument and check it against each item in the array of overrides
-            function checkOverrides(checkThisAgainstPossibleOverrides) {
-                // "list" is an array of our possible overides
-                const list = options.extractors;
-                for (var i = 0, len = list.length; i<len; i++) {
-                    let item = list[i];
-
-                    // If the selector is in the list of overrides return it
-                    if (checkThisAgainstPossibleOverrides.indexOf(item) != -1) {
-                        return true;
-                    }
-                }
-                return false;
-            }
         });
 
+        // List is our array of possible overrides to check against
+        function checkOverrides (list, selector) {
+            for (var i = 0, len = list.length; i < len; i++) {
+                let item = list[i];
+                if (selector.indexOf(item) > -1) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
         // We have our two arrays at this point so write the files out
-        writeExtractedToFile(extracted);
-        writeRemainingToFile(remaining);
-        
-        // Utility functions to write extracted and remaining to disk
+        // writeExtractedToFile(extracted);
+        // writeRemainingToFile(remaining);
+        return Promise.all([writeExtractedToFile(extracted), writeRemainingToFile(remaining)]).then(resolve, reject);
+
         function writeExtractedToFile (extracted) {
-            fs.writeFile(options.extracted, extracted, function (err) {
-                if (err) throw err;
-                
-                // Let"s create a message we can log and write to file
-                const message = `/* ${options.extracted} — size ${fs.statSync(options.extracted).size} bytes/${prettyBytes(gzipSize.sync(fs.readFileSync(options.extracted, 'utf8')))} gzipped, generated on ${fs.statSync(options.extracted).mtime} */ \n`;
-                // Append the message to file
-                prependFile(options.extracted, message, function (err) {});
-                // Log the message
-                console.log("Saved the extracted rules." + message);
+            return new Promise(function (resolve, reject) {
+                fs.writeFile(options.extracted, extracted, function (err) {
+                    if (err) reject(err);
+                    // Let"s create a message we can log and write to file
+                    const message = `/* ${options.extracted} — size ${fs.statSync(options.extracted).size} bytes/${prettyBytes(gzipSize.sync(fs.readFileSync(options.extracted, 'utf8')))} gzipped, generated on ${fs.statSync(options.extracted).mtime} */ \n`;
+                    // Append the message to file
+                    prependFile(options.extracted, message, function (err) {});
+                    // Log the message
+                    console.log("Saved the extracted rules." + message);
+                    resolve();
+                });
             });
-        };
+        }
+
         function writeRemainingToFile (remaining) {
-            fs.writeFile(options.remaining, remaining, function (err) {
-                if (err) throw err;
-                
-                // Let"s create a message we can log and write to file
-                const message = `/* ${options.remaining} — size ${fs.statSync(options.remaining).size} bytes/${prettyBytes(gzipSize.sync(fs.readFileSync(options.remaining, 'utf8')))} gzipped, generated on ${fs.statSync(options.remaining).mtime} */ \n`;
-                // Append the message to file
-                prependFile(options.remaining, message, function (err) {});
-                // Log the message
-                console.log("Saved the remaining rules." + message);
+            return new Promise(function (resolve, reject) {
+                fs.writeFile(options.remaining, remaining, function (err) {
+                    if (err) reject(err);
+                    // Let"s create a message we can log and write to file
+                    const message = `/* ${options.remaining} — size ${fs.statSync(options.remaining).size} bytes/${prettyBytes(gzipSize.sync(fs.readFileSync(options.remaining, 'utf8')))} gzipped, generated on ${fs.statSync(options.remaining).mtime} */ \n`;
+                    // Append the message to file
+                    prependFile(options.remaining, message, function (err) {});
+                    // Log the message
+                    console.log("Saved the remaining rules." + message);
+                    resolve();
+                });
             });
-        };
+        }
+
     }
 });
